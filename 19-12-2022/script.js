6d9bfb5af3c32033d2b65960d65aa65227b76a8f
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2022-12-19 16:25:41 +0100

    Add files via upload

---
+++ 19-12-2022/script.js
@@ -0,0 +1,28 @@
+let operation = prompt (' Inserisci il tipo di operazione che vuoi effettuare: addizione, sottrazione, moltiplicazione o divisione');
+if (operation == 'addizione' || operation == 'sottrazione' || operation == 'moltiplicazione' || operation == 'divisione') {
+let firstNumber = prompt ('Inserisci primo numero');
+let numFirstNumber =parseInt(firstNumber);
+let secondNumber = prompt ('Inserisci secondo numero');
+let numSecondNumber =parseInt(secondNumber);
+if (isNaN(firstNumber) && isNaN(secondNumber) ){
+    alert('Hai inserito un valore non numerico');
+}
+else {
+switch (operation) {
+    case 'addizione':
+        alert (' La somma è ' + (numFirstNumber + numSecondNumber))
+        break;
+    case 'sottrazione':
+        alert (' La differenza è ' + (numFirstNumber - numSecondNumber))
+         break;
+     case 'moltiplicazione':
+        alert (' Il prodotto è ' + (numFirstNumber * numSecondNumber))
+         break;
+     case 'divisione':
+        alert (' Il quoziente è ' + (numFirstNumber / numSecondNumber))
+          break;
+}
+}
+} else {
+    alert ('Non hai inserito un' + 'operazione valida'  )
+}
