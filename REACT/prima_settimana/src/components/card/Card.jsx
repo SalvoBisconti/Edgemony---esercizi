a21050e6da14303c6347633cc74ed75a29ec7ff0
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-03-02 21:38:41 +0100

    esercizio del giorno

--- REACT/prima_settimana/src/components/card/Card.jsx
+++ REACT/prima_settimana/src/components/card/Card.jsx
@@ -6,7 +6,9 @@ const Card = ({ carData, setModal }) => {
   };
   return (
     <div className="card">
-      <img className="img" src={carData.image} alt={carData.character} />
+      <div className="img-sec">
+        <img className="img" src={carData.image} alt={carData.character} />
+      </div>
       <div className="bottom-area">
         <h2 className="name">{carData.character}</h2>
         <button className="quote-btn" onClick={quoteGen}>
