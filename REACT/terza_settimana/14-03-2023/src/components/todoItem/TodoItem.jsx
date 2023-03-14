3c930054e24e375cf457febee641da4ef7a20b78
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-03-14 18:20:16 +0100

    esercizio del giorno

--- REACT/terza_settimana/14-03-2023/src/components/todoItem/TodoItem.jsx
+++ REACT/terza_settimana/14-03-2023/src/components/todoItem/TodoItem.jsx
@@ -1,16 +1,18 @@
 import "./index.css";
 import { BsTrash } from "react-icons/bs";
 
-const TodoItem = ({ todo, setDeletedEl, setArrayEl, deletedEl }) => {
-  const onHandleClick = () => {
-    setDeletedEl(todo.id);
-    setArrayEl((prev) => [...prev.filter((todo) => todo.id !== deletedEl)]);
+const TodoItem = ({ todo, setArrayEl }) => {
+  const onHandleClick = (element) => {
+    setArrayEl((prev) => [...prev.filter((todo) => todo.id !== element)]);
   };
   return (
     <div className="TodoItem">
       <div className="content">
         <p className="todo-text"> {todo.todo}</p>
-        <BsTrash className="trash-icon" onClick={onHandleClick} />
+        <BsTrash
+          className="trash-icon"
+          onClick={() => onHandleClick(todo.id)}
+        />
       </div>
     </div>
   );
--- REACT/terza_settimana/14-03-2023/src/components/todoList/TodoList.jsx
+++ REACT/terza_settimana/14-03-2023/src/components/todoList/TodoList.jsx
@@ -7,7 +7,6 @@ import { useState } from "react";
 const TodoList = ({ setIsModal }) => {
   const [inputSearchEl, setInputSearchEl] = useState("");
   const [arrayEl, setArrayEl] = useState(todos);
-  const [deletedEl, setDeletedEl] = useState(null);
 
   return (
     <div className="TodoList">
@@ -16,20 +15,13 @@ const TodoList = ({ setIsModal }) => {
         setInputSearchEl={setInputSearchEl}
         arrayEl={arrayEl}
         setArrayEl={setArrayEl}
-        deletedEl={deletedEl}
         setIsModal={setIsModal}
       />
 
       {arrayEl
         .sort((a, b) => (a.id >= b.id ? -1 : 0))
         .map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            setDeletedEl={setDeletedEl}
-            setArrayEl={setArrayEl}
-            deletedEl={deletedEl}
-          />
+          <TodoItem key={todo.id} todo={todo} setArrayEl={setArrayEl} />
         ))}
     </div>
   );
